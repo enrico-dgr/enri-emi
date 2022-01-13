6aeb07c2ffe3b082b37c476b04ef40f77c9bc94d
enrico-dgr <elcidwar@gmail.com>
2022-01-13 21:59:50 +0100

    background css fixed

--- src/screens/Registration.js
+++ src/screens/Registration.js
@@ -1,39 +1,33 @@
-import "./registration.css"
+import "./registration.css";
 
-import { Component } from 'react/cjs/react.production.min';
+import { Component } from "react/cjs/react.production.min";
 
 //Components
 
 class Registration extends Component {
-
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            isRegistered: false
-        }
+            isRegistered: false,
+        };
     }
 
     render() {
         return (
             <div className="registration">
-                <div
-                    className="registration-bg">
-                </div>
-                <div className="content">
+                <div className="registration-bg"></div>
+                {/* <div className="content">
                     <h1 className="title">
-                        Rock Scissors Paper<br />&<br /> Lizard Spock
+                        Rock Scissors Paper
+                        <br />&<br /> Lizard Spock
                     </h1>
                     <div className="enter-game">
-                        <input type="text" placeholder="Player Name">
-
-                        </input>
-                        <div className="enter-game__button" >GO!</div>
+                        <input type="text" placeholder="Player Name"></input>
+                        <div className="enter-game__button">GO!</div>
                     </div>
-                    <div
-                        className="img-rules">
-                    </div>
-                </div>
-            </div >
+                    <div className="img-rules"></div>
+                </div> */}
+            </div>
         );
     }
 }
