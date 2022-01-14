abff494ddff4e695e561f31026a8aa910e4be45c
enrico-dgr <elcidwar@gmail.com>
2022-01-14 09:31:26 +0100

    transition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
-import { Component } from "react/cjs/react.production.min";
+import React, { Component } from "react";
+
 //Screens
 import Game from "./screens/Game.js";
 import Registration from "./screens/Registration.js";
@@ -8,14 +9,43 @@ import Registration from "./screens/Registration.js";
 class App extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             isRegistered: false,
+            transition: true,
+            transitionClass: "",
         };
+    }
+
+    transitionOff = () => {
+        setTimeout(() => {
+            this.setState({
+                transition: false,
+            });
+        }, 750);
+    };
+
+    enterAnim = () => {
+        setTimeout(() => {
+            this.setState({
+                transitionClass: "transition-screen-enter",
+            });
+            this.transitionOff();
+        }, 1000);
+    };
+
+    componentDidMount() {
+        this.enterAnim();
     }
 
     render() {
         return (
             <div className="project-enri-emi">
+                {this.state.transition && (
+                    <div
+                        className={`transition-screen ${this.state.transitionClass}`}
+                    ></div>
+                )}
                 {this.state.isRegistered === false && <Registration />}
                 {this.state.isRegistered && <Game />}
             </div>
