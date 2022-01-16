79400f6a074156bd8ab8828f0625ff9332379975
enrico-dgr <elcidwar@gmail.com>
2022-01-16 21:40:01 +0100

    fixed css transitions on landscape

--- src/components/funcComponents/game/ScoreboardTransition.js
+++ src/components/funcComponents/game/ScoreboardTransition.js
@@ -1,12 +1,14 @@
+import "./scoreboardTransition.css";
+
 import PropTypes from "prop-types";
 import TransitionClass from "../../classComponents/Transition";
 
 const ScoreboardTransition = (props) => {
     return (
         <TransitionClass
-            defaultClass={"game-transition-screen"}
-            enterClass={"game-transition-screen--enter"}
-            exitClass={"game-transition-screen--exit"}
+            defaultClass={"scoreboard-transition-screen"}
+            enterClass={"scoreboard-transition-screen--enter"}
+            exitClass={"scoreboard-transition-screen--exit"}
             timeoutBeforeEnter={100}
             timeoutBeforeExit={2000}
             transitionOn={props.transitionOn}
